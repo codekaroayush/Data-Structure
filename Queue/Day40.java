998623d738c7698396b6f9806bc3b0bf3784ca59
codekaroayush <code.karo.ayush@gmail.com>
2026-02-01 17:52:38 +0530

    feat:circular queue using array

---
+++ Queue/Day40.java
@@ -0,0 +1,75 @@
+package Queue;
+
+public class Day40 {
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear;
+        static int front;
+        Queue(int n){
+            arr=new int[n];
+            size=n;
+            rear=-1;
+            front=-1;
+        }
+
+        //isEmpty
+        public static boolean isEmpty(){
+            return rear==-1 && front==-1;
+        }
+    //isFull
+    public static boolean isFull(){
+        return (rear+1)%size==front;
+    }
+
+        //add
+        public static void add(int data){
+            if(isFull()){
+                System.out.println("Queue is Full");
+                return;
+            }if(front==-1){
+                front=0;
+            }
+            rear=(rear+1)%size;
+            arr[rear]=data;
+        }
+
+        //remove
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            
+            int result=arr[front];
+           
+            if(rear==front){
+                front=rear=-1;
+            }else{
+                 front=(front+1)%size;
+            }
+            return result;
+
+        }
+        //peek
+        public static int peek(){
+             if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return arr[front];
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q=new Queue(5);
+        q.add(1);
+        q.add(5);
+        q.add(9);
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+    
+}
