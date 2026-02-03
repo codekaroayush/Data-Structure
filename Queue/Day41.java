79a822b47bd68faa05d2481518a016e9e5265074
codekaroayush <code.karo.ayush@gmail.com>
2026-02-03 22:43:23 +0530

    some changes

--- Queue/Day41.java
+++ Queue/Day41.java
@@ -14,30 +14,12 @@ public class Day41 {
         static Node head=null;
         static Node tail=null;
 
-
-
-        // static int arr[];
-        // static int size;
-        // static int rear;
-        // static int front;
-        // Queue(int n){
-        //     arr=new int[n];
-        //     size=n;
-        //     rear=-1;
-        //     front=-1;
-        // }
-
-        //isEmpty
-        public static boolean isEmpty(){
+        public  boolean isEmpty(){
             return head==null && tail==null;
         }
-    //isFull
-    // public static boolean isFull(){
-    //     return (rear+1)%size==front;
-    // }
-
+        
         //add
-        public static void add(int data){
+        public void add(int data){
            Node newNode=new Node(data);
            if(head==null){
             head=tail=newNode;
@@ -47,7 +29,7 @@ public class Day41 {
         }
 
         //remove
-        public static int remove(){
+        public  int remove(){
             if(isEmpty()){
                 System.out.println("Queue is empty");
                 return -1;
@@ -64,7 +46,7 @@ public class Day41 {
 
         }
         //peek
-        public static int peek(){
+        public int peek(){
              if(isEmpty()){
                 System.out.println("Queue is empty");
                 return -1;
