86334e4053d9d6273502d66937288c1a89a00024
codekaroayush <code.karo.ayush@gmail.com>
2026-02-13 15:36:58 +0530

    some changes

--- Queue/Day46.java
+++ Queue/Day46.java
@@ -1,7 +1,19 @@
 package Queue;
 
+import java.util.*;
+
 public class Day46 {
     public static void main(String[] args) {
-        
+        Deque<Integer> deque=new LinkedList<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addLast(3);
+        deque.addLast(4);
+        System.out.println(deque);
+        deque.removeLast();
+        System.out.println(deque);
+         System.out.println("first element: "+deque.getFirst());
+
+        System.out.println("Last element: "+deque.getLast());
     }
 }
